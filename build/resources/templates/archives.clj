4e4dc5d4ebd0993b39e3886221bd28ac74fdd71b
Isaac Zeng <gaofeng.zeng@tvos.com>
2014-11-28 17:25:18 +0800

    fix bug on github

--- build/resources/templates/archives.clj
+++ build/resources/templates/archives.clj
@@ -4,5 +4,5 @@
     [:h4 {:style "margin-top: 28px;"} date] 
     [:ul {:style "list-style: none; margin-top: 7px;"}
      (for [m ms]
-       [:li [:a {:href (:post-uri m)} (:title m)]])]))]
+       [:li [:a {:href (str "/" (:post-uri m))} (:title m)]])]))]
 
